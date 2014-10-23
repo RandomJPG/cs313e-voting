e47b845a263f2418334efe23c86d6bd6d19fb999
Jacob Garcia <jpgarcia39@yahoo.com>
2014-10-23 02:21:23 -0500

    Fuck you github

---
+++ RunVoting.py
@@ -0,0 +1,30 @@
+# -------------------------------
+# Jacob Garcia
+# jg56492
+# -------------------------------
+
+# -------
+# imports
+# -------
+
+import sys
+
+from Voting import voting_read, election
+
+# ----
+# main
+# ----
+
+def main():
+    numElections = voting_read(sys.stdin)
+    
+    while numElections > 0:
+    
+        elections = election()
+        elections.read(sys.stdin)
+        elections.run()
+        print (elections)
+        
+        numElections -=1
+ 
+main()
